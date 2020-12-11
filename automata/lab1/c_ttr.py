10789a3b21378a1c7a646fd35b4486741e6e9d1f
TrotzigerEmil <roadto3kmmr@gmail.com>
2020-12-11 23:16:48 +0700

    Added nonkeyword token parsing methods

--- automata/lab1/c_ttr.py
+++ automata/lab1/c_ttr.py
@@ -4,14 +4,14 @@ class Dispatcher:
         self.buffer = []
         self.ids = set()
         self.duplicate = None
+        self.nkw = False
 
     def __init__(self):
         self.reset()
 
     def advance(self, s, t):
         self.tok_counter += 1
-        if t == ']':
-            self.tok_counter += 1
+        self.nkw = False
         return True
 
     def wipe(self, s, t):
@@ -25,21 +25,32 @@ class Dispatcher:
             return False
         else:
             self.ids.add(ident)
-            self.tok_counter += 1
             return self.wipe(s, t)
 
     def add_char(self, s, t):
         self.buffer.append(t)
         return True
 
+    def nkw_set(self, s, t):
+        self.nkw = True
+        self.tok_counter += 1
+        return True
+
+    def nkw_err(self, s, t):
+        self.nkw = False
+        return False
+
     def __call__(self, s, t, A):
         actions_map = {'advance': self.advance,
                        'add_char': self.add_char,
-                       'check_id': self.check_id}
+                       'check_id': self.check_id,
+                       'nkw_set': self.nkw_set,
+                       'nkw_err': self.nkw_err,
+                       'wipe': self.wipe}
         return actions_map[A](s, t)
 
     def get_parse_results(self):
-        return {'tok_counter': self.tok_counter,
+        return {'tok_counter': self.tok_counter-int(self.nkw),
                 'duplicate': self.duplicate}
 
 
