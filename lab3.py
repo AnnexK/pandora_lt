40d764d2033cc02f499975e500500d55558d25a0
TrotzigerEmil <roadto3kmmr@gmail.com>
2020-12-21 15:51:18 +0700

    Trying to parse task

--- lab3.py
+++ lab3.py
@@ -1,5 +1,19 @@
 from ll import GrammarParser
 
+
+class Printer:
+    def __init__(self):
+        pass
+
+    def __call__(self, A, t):
+        print(t)
+        return True
+
+    def get_parse_results(self):
+        return dict()
+
+
 G = GrammarParser('test-gram.txt')
-if G.parse('a+a'):
+S = '       namespace    kek       {         namespace lol{     char foo   (int _a[20][30],    char b[72],  int c)    ;   int bar(char c)   {    }}}'
+if G.parse(S):
     print('All ok')
