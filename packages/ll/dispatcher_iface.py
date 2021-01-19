c9f55a94bc291ae64dc14027e6bfbd1ec9aa69da
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-01-20 05:18:11 +0700

    LLDispatcher interface commented

--- packages/ll/dispatcher_iface.py
+++ packages/ll/dispatcher_iface.py
@@ -1,11 +1,18 @@
 from abc import ABCMeta, abstractmethod
 
 
+# интерфейс диспетчера действий для LL(1)-анализатора
 class LLActionDispatcher(metaclass=ABCMeta):
+    # сбросить состояние диспетчера
+    # выполняется до начала разбора анализатором
     @abstractmethod
     def reset(self):
         pass
 
+    # выполнить действие с именем A
+    # в качестве аргумента - текущая лексема из анализируемого потока
+    # (если действие при терминале)
+    # либо следующая лексема
     @abstractmethod
     def __call__(self, A: str, t: str) -> bool:
         pass
