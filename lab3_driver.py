d62d6d4077044dddfc626e701b99d3e21712988f
roadto3kmmr@gmail.com <TrotzigerEmil>
2021-01-20 05:11:48 +0700

    Wrote lab3 driver

--- lab3_driver.py
+++ lab3_driver.py
@@ -1,23 +1,42 @@
-from packages.ll import GrammarParser
-from packages.lab3 import RPNDispatcher
+from packages.ll import GrammarParser, GrammarParserException
+from packages.lab3 import LexDispatcher, SynDispatcher
 from pathlib import Path
 
 
-class Printer:
-    def __init__(self):
-        pass
+def print_tree(t):
+    _print_tree(t, 0)
 
-    def __call__(self, A, t):
-        print(t)
-        return True
+def _print_tree(t, lv):
+    print(f'{"".join("- " for i in range(lv))}{t}')
+    for c in t.children:
+        _print_tree(c, lv+1)
 
-    def reset(self):
-        pass
+with open('input.txt') as fp:
+    s = fp.read()
 
+with open('output.txt', 'w') as fp:
+    L = LexDispatcher()
+    try:
+        A = GrammarParser('packages/lab3/lexg.txt', L)
+        if A.parse(s):
+            stream = L.stream
+            streampos = L.token_pos
+            streampos.append(L.last_char_pos)
+            S = SynDispatcher()
+            B = GrammarParser('ll_grammar.txt', S)
+            if B.parse(stream):
+                fp.write('CORRECT\n')
+            else:
+                err_tok = S.err_tok
+                err_pos = streampos[err_tok]
+                print_tree(S.ns_stack[0])
+                if not S.duplicate:
+                    fp.write(f'INCORRECT {err_pos[0]}:{err_pos[1]}\n')
+                else:
+                    fp.write(f'DUPLICATE {S.duplicate} {err_pos[0]}:{err_pos[1]}')
+        else:
+            err_pos = L.last_char_pos
+            fp.write(f'INCORRECT {err_pos[0]}:{err_pos[1]}')
+    except GrammarParserException as e:
+        fp.write('NOT GRAMMAR\n')
 
-# p = Path('lab3/test-gram.txt').absolute()
-D = RPNDispatcher()
-S = '255+70*2'
-G = GrammarParser('rpn.txt', D)
-if (G.parse(S)):
-    print(D.rpn)
